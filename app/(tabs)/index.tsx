2a78cbbf402ed377d002a4a432339bab53f9f673
pierreglerant <pierre.glerant@student-cs.fr>
2026-04-11 13:53:16 +0200

    feat: Integrate login page in index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,44 @@
-import { useEffect } from 'react';
-import { Text, View } from 'react-native';
-import { supabase } from '@/libs/supabase';
+import { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState<string | null>(null);
+  const router = useRouter();
+
   useEffect(() => {
-    const test = async () => {
-      const { data, error } = await supabase.from('bets').select('*');
-      console.log('DATA:', data);
-      console.log('ERROR:', error);
+    const checkUser = async () => {
+      const storedUser = await AsyncStorage.getItem('user');
+
+      if (!storedUser) {
+        router.replace('/login');
+        return;
+      }
+
+      const user = JSON.parse(storedUser);
+      setUsername(user.username);
+
+      setLoading(false);
     };
-    test();
+
+    checkUser();
   }, []);
 
+  if (loading) {
+    return <Text style={{ marginTop: 100 }}>Loading...</Text>;
+  }
+
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text>Bets & Binouzes</Text>
+    <View style={{ marginTop: 100, padding: 20 }}>
+      <Text style={{ fontSize: 24 }}>
+        Bienvenue {username} 🍻
+      </Text>
+
+      <Text style={{ marginTop: 20 }}>
+        Tu peux maintenant créer des paris 👇
+      </Text>
     </View>
   );
 }
