0d840a9239311e03c0f59c7da0f8a7316cbdc711
pierreglerant <pierre.glerant@student-cs.fr>
2026-04-15 14:54:48 +0200

    feat: Add domain's repositories

---
+++ domain/repositories/BetRepository.ts
@@ -0,0 +1,12 @@
+import { Bet } from '../entities/Bet';
+import { Comment } from '../entities/Comment';
+import { Option } from '../entities/Option';
+
+export interface BetRepository {
+  getBets(): Promise<Bet[]>;
+  getBetById(id: string): Promise<Bet>;
+  getBetWithDetails(id: string): Promise<Bet & {
+    comments: Comment[];
+    options: Option[];
+  }>;
+}
---
+++ domain/repositories/UserRepository.ts
@@ -0,0 +1,6 @@
+
+import { User } from '../entities/User';
+
+export interface UserRepository {
+  getUserByUsername(username: string): Promise<User>;
+}
